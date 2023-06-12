e7e51a7dd12d4450800da13984486a57a2c74b3a
TylerD007 <123567837+TylerD007@users.noreply.github.com>
2023-06-12 19:52:30 -0300

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,3 +1,23 @@
+/*
+ * Pokémon Generation 4 RNG Repository
+ * Written in C++
+
+ * Copyright (C) 2023 Ciro Bogea Fernandes.
+
+ * This library is free software. You can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation;
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+ * Permission to use this software and its documentation *AS IT IS* for any purpose
+ * is hereby granted without fee, provided that the above copyright notice appear 
+ * in all copies and that both that copyright notice and this permission notice appear
+ * in supporting documentation.  It is provided "as is" without express or implied warranty. 
+*/
+
 #include <windows.h>
 #include <stdio.h>
 #include "PKMN_HGSS.h"
